67c810e9a85bc6e1f87a9a1a1296b5a5c51ba5d7
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-10-14 09:45:13 -0600

    Setting variables in the appoiments.php file

--- appointments/appointments.php
+++ appointments/appointments.php
@@ -8,9 +8,6 @@
  */
 
 
-require_once APPOINTMENTS_PLUGIN_PATH . 'admin/inc/admin-functions.php';
-require_once APPOINTMENTS_PLUGIN_PATH . 'appointments/inc/appointments-functions.php';
-require_once APPOINTMENTS_PLUGIN_PATH . 'admin/inc/admin-menu.php';
 
 
 if (!defined('ABSPATH')) {
@@ -19,6 +16,11 @@ if (!defined('ABSPATH')) {
 
 define('APPOINTMENTS_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('APPOINTMENTS_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+require_once APPOINTMENTS_PLUGIN_PATH . 'admin/inc/admin-functions.php';
+require_once APPOINTMENTS_PLUGIN_PATH . 'appointments/inc/appointments-functions.php';
+require_once APPOINTMENTS_PLUGIN_PATH . 'admin/inc/admin-menu.php';
+
 
 register_activation_hook(__FILE__, 'create_appointments_tables');
 register_deactivation_hook(__FILE__, 'drop_appointments_tables');
