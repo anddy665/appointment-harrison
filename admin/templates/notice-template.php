fd9021977b90463948cac43e1256093561cb8728
Harrisong <harrygutie7@gmail.com>
2024-10-25 15:49:38 -0600

    fixing errors

--- admin/templates/notice-template.php
+++ admin/templates/notice-template.php
@@ -1,6 +1,5 @@
-<?php
-if (!empty($message) && !empty($class)) : ?>
-    <div class="notice <?php echo esc_attr($class); ?>">
-        <p><?php echo esc_html($message); ?></p>
+<?php if (!empty($message) && !empty($class)) : ?>
+    <div class="notice <?= esc_attr($class); ?>">
+        <p><?= esc_html($message); ?></p>
     </div>
 <?php endif; ?>
