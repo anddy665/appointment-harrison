5b781cf40b4a539e0babf2ef94aea4858f43c0b5
Harrisong <harrygutie7@gmail.com>
2024-10-29 09:28:27 -0600

    Refactor: Implemented inheritance to reuse loadTemplate and getSchedules in BaseController

--- admin/inc/SchedulesController.php
+++ admin/inc/SchedulesController.php
@@ -1,18 +1,16 @@
 <?php
 
-class SchedulesController {
-    private $wpdb;
+require_once 'BaseController.php';
 
+class SchedulesController extends BaseController {
     public function __construct($wpdb) {
-        $this->wpdb = $wpdb;
+        parent::__construct($wpdb); 
     }
 
     public function handleRequest() {
-  
         $action = isset($_POST['action']) ? sanitize_text_field($_POST['action']) : 'createSchedule';
         $schedule_to_edit = null;
 
-        
         if ($action === 'editSchedule' && isset($_POST['schedule_id'])) {
             $schedule_id = intval($_POST['schedule_id']);
             $schedule_to_edit = $this->wpdb->get_row(
@@ -20,7 +18,6 @@ class SchedulesController {
             );
         }
 
-       
         switch ($action) {
             case 'editSchedule':
                 $this->editSchedule();
@@ -34,21 +31,7 @@ class SchedulesController {
                 break;
         }
 
-       
         $this->loadTemplate($action, $schedule_to_edit);
-    }
-
-    private function loadTemplate($action, $schedule_to_edit) {
-        $schedules = $this->getSchedules();
-        get_template_part('templates/schedules-template', null, [
-            'action' => $action,
-            'schedule_to_edit' => $schedule_to_edit,
-            'schedules' => $schedules,
-        ]);
-    }
-
-    private function getSchedules() {
-        return $this->wpdb->get_results("SELECT * FROM {$this->wpdb->prefix}schedules");
     }
 
     private function createSchedule() {
