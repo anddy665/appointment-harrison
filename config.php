01de8046089d7306dde3f225c3c9c034daf4892f
Harrisong <harrygutie7@gmail.com>
2024-11-11 16:50:51 -0600

    resolving global variables to maintain order

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
-global $wpdb;  
+global $wpdb;
 define('APPOINTMENTS_TABLE', $wpdb->prefix . 'appointments');
 define('SCHEDULES_TABLE', $wpdb->prefix . 'schedules');
 define('APPOINTMENTS_SCHEDULES_TABLE', $wpdb->prefix . 'appointments_schedules');
 
 
 define('SCHEDULES_SLUG', 'schedules');
-define('MENU_SLUG','appointments');
+define('MENU_SLUG', 'appointments');
+
