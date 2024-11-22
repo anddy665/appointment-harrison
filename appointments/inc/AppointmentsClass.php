482ead1f7a3fa01c95112bad251db152313ddab1
Harrisong <harrygutie7@gmail.com>
2024-11-22 10:07:41 -0600

    Refactor: Use global table constants directly in AppointmentDatabaseHandler

--- appointments/inc/AppointmentsClass.php
+++ appointments/inc/AppointmentsClass.php
@@ -15,14 +15,10 @@ class AppointmentDatabaseHandler implements AppointmentDatabaseInterface
 
     public function createTables()
     {
-        $table_appointments = APPOINTMENTS_TABLE;
-        $table_schedules = SCHEDULES_TABLE;
-        $table_appointments_schedules = APPOINTMENTS_SCHEDULES_TABLE;
-
         $charset_collate = $this->wpdb->get_charset_collate();
 
         $sql_appointments = "
-        CREATE TABLE IF NOT EXISTS $table_appointments (
+        CREATE TABLE IF NOT EXISTS " . APPOINTMENTS_TABLE . " (
             id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
             full_name varchar(255) NOT NULL,
             email varchar(100) NOT NULL,
@@ -36,7 +32,7 @@ class AppointmentDatabaseHandler implements AppointmentDatabaseInterface
         ";
 
         $sql_schedules = "
-        CREATE TABLE IF NOT EXISTS $table_schedules (
+        CREATE TABLE IF NOT EXISTS " . SCHEDULES_TABLE . " (
             id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
             schedule_date TINYINT(1) NOT NULL,
             start_time time NOT NULL,
@@ -46,13 +42,13 @@ class AppointmentDatabaseHandler implements AppointmentDatabaseInterface
         ";
 
         $sql_appointments_schedules = "
-        CREATE TABLE IF NOT EXISTS $table_appointments_schedules (
+        CREATE TABLE IF NOT EXISTS " . APPOINTMENTS_SCHEDULES_TABLE . " (
             id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
             appointment_id bigint(20) UNSIGNED NOT NULL,
             schedule_id bigint(20) UNSIGNED NOT NULL,
             PRIMARY KEY (id),
-            FOREIGN KEY (appointment_id) REFERENCES $table_appointments(id) ON DELETE CASCADE,
-            FOREIGN KEY (schedule_id) REFERENCES $table_schedules(id) ON DELETE CASCADE
+            FOREIGN KEY (appointment_id) REFERENCES " . APPOINTMENTS_TABLE . "(id) ON DELETE CASCADE,
+            FOREIGN KEY (schedule_id) REFERENCES " . SCHEDULES_TABLE . "(id) ON DELETE CASCADE
         ) $charset_collate;
         ";
 
@@ -64,11 +60,7 @@ class AppointmentDatabaseHandler implements AppointmentDatabaseInterface
 
     public function dropTables()
     {
-        $table_appointments = APPOINTMENTS_TABLE;
-        $table_schedules = SCHEDULES_TABLE;
-        $table_appointments_schedules = APPOINTMENTS_SCHEDULES_TABLE;
-
-        $sql = "DROP TABLE IF EXISTS $table_appointments_schedules, $table_appointments, $table_schedules;";
+        $sql = "DROP TABLE IF EXISTS " . APPOINTMENTS_SCHEDULES_TABLE . ", " . APPOINTMENTS_TABLE . ", " . SCHEDULES_TABLE . ";";
         $this->wpdb->query($sql);
     }
 }
