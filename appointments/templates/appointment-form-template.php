a78eecd6ca88a753f6fd01e62ad59c7de1213324
Harrisong <harrygutie7@gmail.com>
2024-11-25 16:30:57 -0600

    first styles

--- appointments/templates/appointment-form-template.php
+++ appointments/templates/appointment-form-template.php
@@ -5,23 +5,48 @@
 
     <form id="appointment-form" method="POST">
         <?php wp_nonce_field('submit_appointment_form', 'appointment_form_nonce'); ?>
-        <input type="text" name="full_name" placeholder="Full Name" required>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="text" name="phone" placeholder="Phone" required>
-        <textarea name="description" placeholder="Description"></textarea>
 
-        <label for="appointment_date">Select Date:</label>
-        <input type="date" id="appointment_date" name="appointment_date" required>
+        <div class="form-group">
+            <label for="full_name">Full Name:</label>
+            <input type="text" id="full_name" name="full_name" placeholder="Full Name" required>
+        </div>
 
-        <label for="start_time">Start Time:</label>
-        <input type="time" id="start_time" name="start_time" required>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Email" required>
+        </div>
 
-        <label for="end_time">End Time:</label>
-        <input type="time" id="end_time" name="end_time" required>
+        <div class="form-group">
+            <label for="phone">Phone:</label>
+            <input type="text" id="phone" name="phone" placeholder="Phone" required>
+        </div>
+
+        <div class="form-group full-width">
+            <label for="description">Description:</label>
+            <textarea id="description" name="description" placeholder="Description"></textarea>
+        </div>
+
+
+
+        <div class="form-group">
+            <label for="start_time">Start Time:</label>
+            <input type="time" id="start_time" name="start_time" required>
+        </div>
+
+        <div class="form-group">
+            <label for="end_time">End Time:</label>
+            <input type="time" id="end_time" name="end_time" required>
+        </div>
+
+        <div class="form-group">
+            <label for="appointment_date">Select Date:</label>
+            <input type="date" id="appointment_date" name="appointment_date" required>
+        </div>
 
         <button type="submit">Book Appointment</button>
     </form>
-    <div id="error-message" class="error-message" style="display: none;">
+
+    <div id="error-message" class="error-message">
         <span id="error-text"></span>
     </div>
 </div>
